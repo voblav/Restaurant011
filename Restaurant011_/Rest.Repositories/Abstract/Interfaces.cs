c5bcfbebdb38dfe6fda5860c1a77ff570d350618
voblav <35613203+voblav@users.noreply.github.com>
2018-06-15 10:49:28 +0300

    Update

--- Restaurant011_/Rest.Repositories/Abstract/Interfaces.cs
+++ Restaurant011_/Rest.Repositories/Abstract/Interfaces.cs
@@ -1,10 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using Rest.DomainAbstractions.Repositoriess;
+using Rest.Entities.Code;
+
 
 namespace Rest.Repositories.Abstract
 {
-    interface Interfaces
+    public interface IDishesRepository : IDbRepository<Dish>
+    {
+    }
+    public interface ICompositionsRepository : IDbRepository<Composition>
+    {
+    }
+    public interface IIngredientsRepository : IDbRepository<Ingredient>
+    {
+    }
+    public interface IOrdersRepository : IDbRepository<Order>
+    {
+    }
+    public interface IOrderFromMenuesRepository : IDbRepository<OrderFromMenu>
+    {
+    }
+    public interface IPurchasersRepository : IDbRepository<Purchaser>
+    {
+    }
+    public interface IStewardsRepository : IDbRepository<Steward>
+    {
+    }
+    public interface ISuppliesRepository : IDbRepository<Supple>
     {
     }
 }
+
