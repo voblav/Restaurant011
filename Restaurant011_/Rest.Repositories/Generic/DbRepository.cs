ccb0273d12925dccee2474c65a82773df3c7246a
voblav <35613203+voblav@users.noreply.github.com>
2018-06-15 10:50:45 +0300

    Update

--- Restaurant011_/Rest.Repositories/Generic/DbRepository.cs
+++ Restaurant011_/Rest.Repositories/Generic/DbRepository.cs
@@ -1,10 +1,78 @@
-﻿using System;
+using Rest.AppContext.Code;
+using Rest.DomainAbstractions.Entities;
+using Rest.DomainAbstractions.Repositoriess;
+using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
+
 
 namespace Rest.Repositories.Generic
 {
-    class DbRepository
+    public class DbRepository<T> : IDbRepository<T> 
+        where T : class, IDbEntity
     {
+        private MyAppDbContext _context;
+
+        public DbRepository(MyAppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IQueryable<T> AllItems
+        {
+            get
+            {
+                return _context.Set<T>();
+            }
+        }
+
+        public bool AddItem(T item)
+        {
+            _context.Set<T>().Add(item);
+            return SaveChanges();
+        }
+
+        public bool AddItems(IEnumerable<T> items)
+        {
+            _context.Set<T>().AddRange(items);
+            return SaveChanges();
+        }
+
+        public bool ChangeItem(T item)
+        {
+            T changed = GetItem(item.Id);
+            if (changed == null) return false;
+            changed = item;
+            return SaveChanges();
+        }
+
+        public bool DeleteItem(Guid id)
+        {
+            T item = GetItem(id);
+            if (item == null)
+                return false;
+            _context.Set<T>().Remove(item);
+            return true;
+        }
+
+        public T GetItem(Guid id)
+        {
+            return AllItems.FirstOrDefault(x => x.Id.Equals(id));
+        }
+
+        public bool SaveChanges()
+        {
+            try
+            {
+                _context.SaveChanges();
+                return true;
+            }
+#pragma warning disable 0168
+            catch (Exception e)
+#pragma warning restore 0168
+            {
+                return false;
+            }
+        }
     }
 }
