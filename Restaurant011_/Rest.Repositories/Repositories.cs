ec3ddb82ee15c7b40efeab32fa6bfdbd7d2da7bd
voblav <35613203+voblav@users.noreply.github.com>
2018-06-15 10:47:20 +0300

    Update

--- Restaurant011_/Rest.Repositories/Repositories.cs
+++ Restaurant011_/Rest.Repositories/Repositories.cs
@@ -4,8 +4,75 @@ using System.Text;
 
 namespace Rest.Repositories
 {
-    class Repositories
+    public class DishesRepository :DbRepository<Dish>, IDishesRepository
     {
-        
+        public DishesRepository(MyAppDbContext context)
+            : base(context)
+        {
+
+        }
+
+        public class CompositionsRepository : DbRepository<Composition>, ICompositionsRepository
+        {
+            public CompositionsRepository(MyAppDbContext context)
+                : base(context)
+            {
+
+            }
+        }
+
+        public class IngredientsRepository : DbRepository<Ingredient>, IIngredientsRepository
+        {
+            public IngredientsRepository(MyAppDbContext context)
+                : base(context)
+            {
+
+            }
+        }
+
+        public class OrdersRepository : DbRepository<Order>, IOrdersRepository
+        {
+            public OrdersRepository(MyAppDbContext context)
+                : base(context)
+            {
+
+            }
+        }
+
+        public class OrderFromMenuesRepository : DbRepository<OrderFromMenu>, IOrderFromMenuesRepository
+        {
+            public OrderFromMenuesRepository(MyAppDbContext context)
+                : base(context)
+            {
+
+            }
+        }
+
+        public class PurchasersRepository : DbRepository<Purchaser>, IPurchasersRepository
+        {
+            public PurchasersRepository(MyAppDbContext context)
+                : base(context)
+            {
+
+            }
+        }
+
+        public class StewardRepository : DbRepository<Steward>, IStewardsRepository
+        {
+            public StewardRepository(MyAppDbContext context)
+                : base(context)
+            {
+
+            }
+        }
+
+        public class SuppleRepository : DbRepository<Supple>, ISuppliesRepository
+        {
+            public SuppleRepository(MyAppDbContext context)
+                : base(context)
+            {
+
+            }
+        }
     }
 }
