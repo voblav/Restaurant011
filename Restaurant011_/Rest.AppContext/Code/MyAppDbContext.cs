8fe42815e2b0dacf6d496c6aeae16b3f108f237d
voblav <35613203+voblav@users.noreply.github.com>
2018-06-15 10:35:13 +0300

    Update

--- Restaurant011_/Rest.AppContext/Code/MyAppDbContext.cs
+++ Restaurant011_/Rest.AppContext/Code/MyAppDbContext.cs
@@ -12,5 +12,20 @@ namespace Rest.AppContext.Code
     {
         public DbSet<Composition> Compositions { get; set; }
         public DbSet<Dish> Dishes { get; set; }
+        public DbSet<Ingredient> Ingredients { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderFromMenu> OrderFromMenues { get; set; }
+        public DbSet<Purchaser> Purchasers { get; set; }
+        public DbSet<Steward> Stewards { get; set; }
+        public DbSet<Supple> Suppliers { get; set; }
+
+        public MyAppDbContext(string nameOrConnectionString)
+            : base(nameOrConnectionString)
+        {
+        }
+        static MyAppDbContext()
+        {
+            Database.SetInitializer(new MyAppDbContextInitializer());
+        }
     }
 }
