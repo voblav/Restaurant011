862e31e34f474f0bca7be4da3ca8df4ac589abd6
voblav <35613203+voblav@users.noreply.github.com>
2018-06-15 07:03:45 +0300

    Updating

--- Restaurant011_/Rest.DataUnit/Unit.cs
+++ Restaurant011_/Rest.DataUnit/Unit.cs
@@ -8,5 +8,25 @@ namespace Rest.DataUnit
     public static class Unit
     {
         static MyAppDbContext _context;
+        public static ICompositionsRepository CompositionsRepository { get; private set; }
+        public static IDishesRepository DishesRepository { get; private set; }
+        public static IIngredientsRepository IngredientsRepository { get; private set; }
+        public static IOrdersRepository OrdersRepository { get; private set; }
+        public static IOrderFromMenuesRepository OrderFromMenuesRepository { get; private set; }
+        public static IPurchasersRepository PurchasersRepository { get; private set; }
+        public static IStewardsRepository StewardsRepository { get; private set; }
+        public static ISuppliesRepository SuppliesRepository { get; private set; }
+        static Unit()
+        {
+            _context = new MyAppDbContext("DbRest");
+            DishesRepository = new DishesRepository(_context);
+            CompositionsRepository = new CompositionsRepository(_context);
+            IngredientsRepository = new IngredientsRepository(_context);
+            OrdersRepository = new OrdersRepository(_context);
+            OrderFromMenuesRepository = new OrderFromMenuesRepository(_context);
+            PurchasersRepository = new PurchasersRepository(_context);
+            StewardsRepository = new StewardsRepository(_context);
+            SuppliesRepository = new SuppliesRepository(_context);
+        }
     }
 }
